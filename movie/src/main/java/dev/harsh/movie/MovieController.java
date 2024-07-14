8b9c4b5e772bfd2fa5b13db639d76f7da3b33c00
harshxo <shrivastavaharsh876@gmail.com>
2024-07-14 14:41:51 +0530

    final changes for review error

--- movie/src/main/java/dev/harsh/movie/MovieController.java
+++ movie/src/main/java/dev/harsh/movie/MovieController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+
 @RequestMapping("/api/v1/movies")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MovieController {
 	@Autowired
 	private MovieService movieService;
